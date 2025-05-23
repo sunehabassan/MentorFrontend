cf34d17b1ecf2caa0e3b2c80dfa039334b1446aa
suneha <sunehabassan9@gmail.com>
2025-05-23 14:24:19 +0530

    protected routes

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { GoogleOAuthProvider } from '@react-oauth/google';
 import './index.css';
+
 import Login from './Login.jsx';
 import MentorCard from './Student/MentorCard.jsx';
 import Home from './Home.jsx';
@@ -15,7 +16,6 @@ import Apitesting from './Apitesting.jsx';
 import NotFound from './NotFound.jsx';
 import GoogleLogin from './GoogleLogin.jsx';
 import RefrshHandler from './RefreshHandler';
-// import Teacherprofile from './Teacherprofile.jsx';
 import Studentlog from './Login/Studentlog.jsx';
 import Mentorlog from './Login/Mentorlog.jsx';
 import Courses from './Mentor/Courses.jsx';
@@ -33,7 +33,6 @@ import BenefitsSection from './components/BenefitsSection.jsx';
 import FAQSection from './components/FAQSection.jsx';
 import TestimonialSection from './components/TestimonialSection.jsx';
 import HowItWorks from './components/HowItWorks.jsx';
-
 import LearningMethods from './components/LearningMethods.jsx';
 import MentorProfile from './Mentor/Mentorprofile.jsx';
 import VideoCall from './Communication/VideoCall.jsx';
@@ -42,12 +41,15 @@ import EmailCommunication from './Communication/EmailCommunication';
 import AdminFaqAnswer from './AdminFaqAnswer.jsx';
 import Feedbacks from './Student/Feedbacks.jsx';
 import MentorProfileForm from './Mentor/INFO/Wrapper.jsx';
-import { StudentProfileForm, StudentProfileView } from './Student/studentProfile.jsx'
+import { StudentProfileForm, StudentProfileView } from './Student/studentProfile.jsx';
 import MentorRequests from './Mentor/INFO/getUpdateRequests.jsx';
 import Contactstu from './Student/Contactstu.jsx';
 import Contactment from './Mentor/Contactment.jsx';
 import AdminFaqDashboard from './adminfaq.jsx';
 import Feedback from './Mentor/Feedback.jsx';
+
+import ProtectedRoute from './components/ProtectedRoute.jsx';
+
 const Main = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
 
@@ -56,62 +58,95 @@ const Main = () => {
       <BrowserRouter>
         <RefrshHandler setIsAuthenticated={setIsAuthenticated} />
         <Routes>
-        <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/googlelogin" element={<GoogleLogin />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/MentorCard" element={<MentorCard />} />
           <Route path="/student" element={<Student />} />
           <Route path="/Header" element={<Header />} />
           <Route path="/Footer" element={<Footer />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/aboutus" element={<Aboutus />} />
           <Route path="/apitesting" element={<Apitesting />} />
-          <Route path="/VideoCall" element={<VideoCall/>} />
-          <Route path="/VoiceCall" element={<VoiceCall/>} />
-          <Route path="/EmailCommunication" element={<EmailCommunication/>} />
-          <Route path="//AdminFaqAnswer/:id" element={<AdminFaqAnswer/>} />
-          <Route path="/MentorProfileForm" element={<MentorProfileForm/>} />
+          <Route path="/VideoCall" element={<VideoCall />} />
+          <Route path="/VoiceCall" element={<VoiceCall />} />
+          <Route path="/EmailCommunication" element={<EmailCommunication />} />
+          <Route path="/AdminFaqAnswer/:id" element={<AdminFaqAnswer />} />
+          <Route path="/Studentlog" element={<Studentlog />} />
+          <Route path="/Mentorlog" element={<Mentorlog />} />
+          <Route path="/Walpaperslider" element={<Walpaperslider />} />
+          <Route path='/Headersec' element={<Headersec />} />
+          <Route path='/Button1' element={<Button1 />} />
+          <Route path='/App2' element={<App2 />} />
+          <Route path='/BenefitsSection' element={<BenefitsSection />} />
+          <Route path='/FAQSection' element={<FAQSection />} />
+          <Route path='/TestimonialSection' element={<TestimonialSection />} />
+          <Route path='/HowItWorks' element={<HowItWorks />} />
+          <Route path='/LearningMethods' element={<LearningMethods />} />
+          <Route path='/AdminFaqDashboard' element={<AdminFaqDashboard />} />
+          <Route path='/MentorProfile' element={<MentorProfile />} />
 
-          <Route path="/MentorRequests" element={<MentorRequests/>} />
-          <Route path="/Contactstu" element={<Contactstu/>} />
-       <Route path="/Contactment" element={<Contactment/>}/>
-          <Route path="/AdminFaqDashboard" element={<AdminFaqDashboard/>} />
-        
-          <Route path="/Studentlog" element={<Studentlog/>}/>
-          <Route path="/Mentorlog" element={<Mentorlog/>}/>
-          <Route path="/Courses" element={<Courses/>}/>
-<Route path="/Mentorheader" element={<Mentorheader/>}/>
-          <Route path="/FetchCourses" element={<FetchCourses/>}/>
-    <Route path="/Homementor" element={<Homementor/>}/>
-    <Route path="/Studentheader" element={<Studentheader/>}/>
-    <Route path="/Homestu" element={<Homestu/>}/>
-    <Route path="/Mystudents" element={<Mystudents/>}/>
-    <Route path="/Walpaperslider" element={<Walpaperslider/>}/>
-    <Route path='/Headersec' element={<Headersec/>}/>
-<Route path='/Button1' element={<Button1/>}/>
-<Route path='/App2' element={<App2/>}/>
-<Route path='/BenefitsSection' element={<BenefitsSection/>}/>
-<Route path='/FAQSection' element={<FAQSection/>}/>
-<Route path='/TestimonialSection ' element={<TestimonialSection />}/>
-<Route path='/HowItWorks' element={<HowItWorks />}/>
-<Route path='/LearningMethods ' element={<LearningMethods />}/>
+          {/* ✅ Protected Routes */}
+          <Route path="/MentorCard" element={
+            <ProtectedRoute><MentorCard /></ProtectedRoute>
+          } />
+          <Route path="/MentorRequests" element={
+            <ProtectedRoute><MentorRequests /></ProtectedRoute>
+          } />
+          <Route path="/MentorProfileForm" element={
+            <ProtectedRoute><MentorProfileForm /></ProtectedRoute>
+          } />
+          <Route path="/Contactstu" element={
+            <ProtectedRoute><Contactstu /></ProtectedRoute>
+          } />
+          <Route path="/Contactment" element={
+            <ProtectedRoute><Contactment /></ProtectedRoute>
+          } />
+          <Route path="/Courses" element={
+            <ProtectedRoute><Courses /></ProtectedRoute>
+          } />
+          <Route path="/Mentorheader" element={
+            <ProtectedRoute><Mentorheader /></ProtectedRoute>
+          } />
+          <Route path="/Homementor" element={
+            <ProtectedRoute><Homementor /></ProtectedRoute>
+          } />
+          <Route path="/FetchCourses" element={
+            <ProtectedRoute><FetchCourses /></ProtectedRoute>
+          } />
+          <Route path="/Studentheader" element={
+            <ProtectedRoute><Studentheader /></ProtectedRoute>
+          } />
+          <Route path="/Homestu" element={
+            <ProtectedRoute><Homestu /></ProtectedRoute>
+          } />
+          <Route path="/Feedbacks" element={
+            <ProtectedRoute><Feedbacks /></ProtectedRoute>
+          } />
+          <Route path="/Feedback" element={
+            <ProtectedRoute><Feedback /></ProtectedRoute>
+          } />
 
-<Route path='/Feedbacks' element={<Feedbacks/>}/>
-<Route path='/Feedback' element={<Feedback/>}/>
-
-<Route path="/StudentProfileForm" element={<StudentProfileForm token={localStorage.getItem('Token')} />} />
-<Route path="/StudentProfileView" element={<StudentProfileView token={localStorage.getItem('Token')} />} />
-
-
-<Route path='/MentorProfile' element={<MentorProfile/>}/>
-
+          {/* Optional: Student Profile Routes (also typically protected) */}
+          <Route path="/StudentProfileForm" element={
+            <ProtectedRoute>
+              <StudentProfileForm token={localStorage.getItem('Token')} />
+            </ProtectedRoute>
+          } />
+          <Route path="/StudentProfileView" element={
+            <ProtectedRoute>
+              <StudentProfileView token={localStorage.getItem('Token')} />
+            </ProtectedRoute>
+          } />
 
           <Route path="*" element={<NotFound />} />
         </Routes>
       </BrowserRouter>
     </GoogleOAuthProvider>
-    
   );
 };
 
-createRoot(document.getElementById('root')).render(<StrictMode><Main /></StrictMode>);
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Main />
+  </StrictMode>
+);
