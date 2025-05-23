7e10ac9016b2ca7e60d96bf159a32a30343f6ea9
suneha <sunehabassan9@gmail.com>
2025-05-23 16:31:31 +0530

    error solve

--- vite.config.js
+++ vite.config.js
@@ -8,4 +8,10 @@ export default defineConfig({
   tailwindcss(),
   // reactRouter(),
   ],
+  server: {
+    host: '0.0.0.0',
+    port: process.env.PORT ? parseInt(process.env.PORT) : 5173,
+    allowedHosts: ['trendify-frontend-22gf.onrender.com']
+
+  }
 })
